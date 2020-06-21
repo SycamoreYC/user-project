2dbe547d394f022c43d4b58f49ff8ffdee049606
Yechun Song <yechun.song@rea-group.com>
2020-06-21 20:45:12 +0800

    add docker-compose and flyway

---
+++ src/main/resources/db/migration/V1__Create_user_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `t_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) character set utf8 default NULL,
+  `name` int(11) default NULL,
+  `createdAt` datetime default NULL,
+  `updatedAt` datetime default NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
