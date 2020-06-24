306a120e002e86806b637d4e27de388628aa728f
Qinyun Wang <oojbjoo@gmail.com>
2020-06-24 13:44:46 +0800

    Configurate jacoco to check test coverage

--- src/test/java/com/java/userproject/service/UserServiceTest.java
+++ src/test/java/com/java/userproject/service/UserServiceTest.java
@@ -1,13 +1,17 @@
 package com.java.userproject.service;
 
+import com.java.userproject.errors.UserNotFoundException;
 import com.java.userproject.model.User;
 import com.java.userproject.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -16,6 +20,13 @@ class UserServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserService(userRepository);
+    }
+
     @Test
     void should_returnUser_when_getUserSuccessfully() {
         User mockUser = User.builder()
@@ -23,10 +34,33 @@ class UserServiceTest {
                 .age(18)
                 .name("test")
                 .build();
+
         when(userRepository.findById(1L)).thenReturn(java.util.Optional.ofNullable(mockUser));
 
-        UserService userService = new UserService(userRepository);
         User actualUser = userService.getUser(1L);
         assertThat(actualUser).isEqualTo(mockUser);
+    }
+
+    @Test
+    void should_throwException_when_getUserFailed() {
+        when(userRepository.findById(1L)).thenThrow(UserNotFoundException.class);
+
+        assertThatThrownBy(() -> userService.getUser(1L)).isInstanceOf(UserNotFoundException.class);
+    }
+
+    @Test
+    void should_returnTrue_when_deleteUserSuccessfully() {
+        Boolean result = userService.deleteUser(1L);
+
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    void should_returnFalse_when_deleteUserFailed() {
+        doThrow(IllegalArgumentException.class).when(userRepository).deleteById(1L);
+
+        Boolean result = userService.deleteUser(1L);
+
+        assertThat(result).isEqualTo(false);
     }
 }
