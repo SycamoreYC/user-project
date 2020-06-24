bcdc4f644fae623963bb03042ef8c5829bffaad9
Yechun Song <yechun.song@rea-group.com>
2020-06-24 17:10:14 +0800

    Add User and Api actuator

--- src/main/java/com/java/userproject/controller/UserController.java
+++ src/main/java/com/java/userproject/controller/UserController.java
@@ -4,18 +4,10 @@ import com.java.userproject.model.User;
 import com.java.userproject.model.UserRequest;
 import com.java.userproject.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -26,6 +18,8 @@ import java.util.List;
 public class UserController {
 
     private final UserService userService;
+
+    @Qualifier(value = "userCounter")
 
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
